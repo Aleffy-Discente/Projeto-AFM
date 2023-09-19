40a39d64cb5c4870e0bc1980926a8fe3e281d6af
Aleffy-Discente <143960299+Aleffy-Discente@users.noreply.github.com>
2023-09-18 21:05:34 -0300

    aTestes

--- testLog.php
+++ testLog.php
@@ -1,33 +1,22 @@
 <?php 
 
-    //print_r($_REQUEST);
+    print_r($_REQUEST);
     if(isset($_POST['submit']) && !empty($_POST['senha']) && !empty($_POST['email']))
     {
-        include_once('config.php');
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
-
-        //print_r('Email: ' . $email);
-        //print_r('Email: ' . $senha);
-
-        $sgl = "SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'";
-
-        $result = $conexao->query($sql);
-        //print_r($result);
-        //print_r($sql);
-        if(mysqli_num_rows($result) < 1)
-        {
-            print_r('Não existe');
-        }
-        else
-        {
-            print_r('Existe');
-        }
-
+        header('Location: log.php');
     }
     else
     {
-        header('location: log.php');
+        
+        include_once('config.php');
+        $email = $_POST['email'];
+        print_r('<br>');
+        $senha = $_POST['senha'];
+
+        print_r('Email: ' . $email);
+        print_r('<br>');
+        print_r('Senha: ' . $senha);
     }
+    
 
 ?>
