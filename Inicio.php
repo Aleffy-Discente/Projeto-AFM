29791db74a49bd229d4f24f23f034de02d2d9d08
Aleffy-Discente <143960299+Aleffy-Discente@users.noreply.github.com>
2023-09-22 19:37:06 -0300

    Agora é só perfumar

---
+++ Inicio.php
@@ -0,0 +1,25 @@
+<<?php 
+    session_start();
+    //print_r($_SESSION);
+    if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true))
+    {
+        unset($_SESSION['email']);
+        unset($_SESSION['senha']);
+        header('Location: log.php');
+    }
+    $logado = $_SESSION['email'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio | AFM</title>
+</head>
+<body>
+    <a href="sair.php">Sair</a>
+    <h1>Acessou o sistema!</h1>
+    
+</body>
+</html>
--- testLog.php
+++ testLog.php
@@ -1,5 +1,5 @@
 <?php 
-
+    session_start();
     //print_r($_REQUEST);
     if(isset($_POST['submit'])) //&& !empty($_POST['email']) && !empty($_POST['senha']))
     {
@@ -7,12 +7,32 @@
         $email = $_POST['email'];
         $senha = $_POST['senha'];
 
-        print_r('Email: ' . $email);
-        print_r('<br>');
-        print_r('Senha: ' . $senha);
+        //print_r('Email: ' . $email);
+        //print_r('<br>');
+        //print_r('Senha: ' . $senha);
+
+        $sql = "SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'"; 
+
+        $result = $conexao->query($sql);
+        //print_r($result);
+        //print_r($sql);
+        
+        if(mysqli_num_rows($result) < 1)
+        {
+            unset($_SESSION['email']);
+            unset($_SESSION['senha']);
+            header('Location: log.php');
+        }
+        else
+        {
+            $_SESSION['email'] = $email;
+            $_SESSION['senha'] = $senha;
+           header('Location: Inicio.php');
+        }
     }
     else
     {
+        
         header('Location: log.php');
     }
     
