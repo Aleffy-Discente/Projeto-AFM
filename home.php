a36c58421db60f9a98e0a7d63fb2521127d095ce
Aleffy-Discente <143960299+Aleffy-Discente@users.noreply.github.com>
2023-09-22 21:18:31 -0300

    Aqui que começo a fazer besteira

---
+++ home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StudyPlannerAfricam</title>
+</head>
+<body>
+    <header>
+        <div id="title">
+            <h1>StudyPlanner</h1>
+            <h1>Africam</h1>
+        </div>
+        <ul>
+            <a href="a"><li>Inicio</li></a>
+            <a href="a"><li>Contato</li></a>
+            <a href="a"><li>Sobre</li></a>
+            <a href="a"><li>Serviços</li></a>
+            <a href="a"><li>Já tem uma conta?</li></a>
+        </ul>
+    </header>
+
+    <main>
+        <aside>
+            <h2>Agende sua atividade aqui</h2>
+            <h2>no melhor agendador do continente</h2>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+        </aside>
+
+    </main>
+</body>
+</html>
