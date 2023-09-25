a0428db215972a82a30335405a66cbb985bbd518
Aleffy-Discente <143960299+Aleffy-Discente@users.noreply.github.com>
2023-09-25 20:12:46 -0300

    Só falta colocar a barra de pesquisa

--- Inicio.php
+++ Inicio.php
@@ -79,7 +79,7 @@ if(isset($_POST['submit']))
             height: 98vh;  
             background-image: url("Imagens/Logo_Atulizada-removebg-preview.png");
             background-position: left top;
-            margin-right: 60px;
+            
             background-repeat: no-repeat;
             background-size: 200px;
             background-color: #F0F2F5;
@@ -232,6 +232,16 @@ if(isset($_POST['submit']))
         {
             background-position: center top;
         }
+        .box-search
+        {
+            display: flex;
+            justify-content: center;
+            gap: 1%;
+        }
+        button
+        {
+            margin-bottom: 30px;
+        }
     </style>
     <!-- Código -->
 </head>
@@ -266,6 +276,14 @@ if(isset($_POST['submit']))
             </form>
         </div>
     </header>
+    <div class="box-search">
+        <input type="search" class="form-control w-25" placeholder="Pesquisar" id="pesquisar">
+        <button onclick="searchData()" class="btn btn-primary">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+            </svg>
+        </button>
+    </div>
     <table>
         <thead>
             <tr>
